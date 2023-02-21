9db104a1c674c4aa0c0dd04a428cea55eb02bb1e
BlokhinaGalina <galina-a@yandex.ru>
2023-02-21 23:20:29 +0300

    added 9-2 file

---
+++ 9-2.js
@@ -0,0 +1,9 @@
+const calculateBonus = (a, b) => {
+let bonus;
+const sum = a + b;
+debugger; //определены значения a и b, рассчитана сумма
+sum > 50 ? (bonus = 50) : (bonus = sum);
+debugger; // выполнено условие: бонус равен сумме
+return bonus;
+};
+console.log(calculateBonus(10,20))
